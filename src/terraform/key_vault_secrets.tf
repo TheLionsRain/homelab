1b82aa9b830d6771d17e7f2b2afdc8754bb1797a
Michiel Van Herreweghe <169037533+MichielVanHerreweghe@users.noreply.github.com>
2025-05-21 10:53:57 +0200

    fix: add depends_on statement

--- src/terraform/key_vault_secrets.tf
+++ src/terraform/key_vault_secrets.tf
@@ -15,10 +15,16 @@ resource "azurerm_key_vault_secret" "st_backup_name_secret" {
   name         = var.st_backup_name_secret_name
   value        = azurerm_storage_account.st_backup.name
   key_vault_id = azurerm_key_vault.kv.id
+  depends_on = [
+    azurerm_role_assignment.roles_kv
+  ]
 }
 
 resource "azurerm_key_vault_secret" "st_backup_access_key_secret" {
   name         = var.st_backup_access_key_secret_name
   value        = azurerm_storage_account.st_backup.primary_access_key
   key_vault_id = azurerm_key_vault.kv.id
+  depends_on = [
+    azurerm_role_assignment.roles_kv
+  ]
 }
